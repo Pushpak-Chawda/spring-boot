92aceec4805cb2cbd79db9bb956b98b7b6821d0b
Pushpak-Chawda <pushpaktcas@gmail.com>
2024-10-17 23:00:49 -0400

    Initial commit

---
+++ pc.main.web/src/main/java/com/pc/main/web/PcMainApplication.java
@@ -0,0 +1,13 @@
+package com.pc.main.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcMainApplication.class, args);
+	}
+
+}
---
+++ pc.main.web/src/test/java/com/pc/main/web/PcMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc.main.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
