1a57587f6f767f2b87cd1d1ca3feea19fa161f26
pushp <pushp@10.0.0.83>
2025-10-05 22:51:07 -0400

    Initial Commit

---
+++ SpringAOPEx/src/test/java/com/spring/core/aop/SpringAopExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.core.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
